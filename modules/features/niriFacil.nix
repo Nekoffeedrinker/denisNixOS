927d38593fcb9bf64906db6041ade068573d92bd
Denis Castillón <orangeneko158@outlook.com>
2026-04-13 09:56:17 -0700

    niri: añadir portal y temas

--- modules/features/niriFacil.nix
+++ modules/features/niriFacil.nix
@@ -3,6 +3,14 @@
     programs.niri.enable = true;
     environment.systemPackages = [
       inputs.noctalia.packages.${pkgs.stdenv.hostPlatform.system}.default
+      pkgs.adwaita-icon-theme
+      pkgs.hicolor-icon-theme
+    ];
+	# activar portal (interfaz para que apps en 
+	# Wayland accedan a archivos, pantalla, etc.)
+    xdg.portal.enable = true;
+    xdg.portal.extraPortals = with pkgs; [
+      xdg-desktop-portal-gtk
     ];
   };
 }
