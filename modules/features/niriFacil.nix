3d8181d808eb8154e680994bd83ef3589f6af8a5
Denis Castillón <orangeneko158@outlook.com>
2026-04-14 16:55:02 -0700

    "fix: quitar el systemd de dms"
    Se retiró esta configuración porque también se ejecutaba con GNOME, lo
    cual es indeseado porque solo se usa en Niri.

--- modules/features/niriFacil.nix
+++ modules/features/niriFacil.nix
@@ -20,11 +20,6 @@
       enable = true;
       quickshell.package = inputs.quickshell.packages.${pkgs.stdenv.hostPlatform.system}.quickshell;
 
-      systemd = {
-        enable = true; # Systemd service for auto-start
-        restartIfChanged = true; # Auto-restart dms.service when dms-shell changes
-      };
-
       # Core features
       enableSystemMonitoring = true; # System monitoring widgets (dgop)
       enableVPN = true; # VPN management widget
