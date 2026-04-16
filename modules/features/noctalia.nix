4194930caf5bdc8aa4d24f5738f756318eb7be4f
Denis Castillón <orangeneko158@outlook.com>
2026-04-15 17:05:32 -0700

    fix: falta quickshell

--- modules/features/noctalia.nix
+++ modules/features/noctalia.nix
@@ -2,6 +2,7 @@
   flake.nixosModules.denisNoctalia = {pkgs, ...}: {
     environment.systemPackages = [
       inputs.noctalia.packages.${pkgs.stdenv.hostPlatform.system}.default
+      pkgs.quickshell
     ];
 
     programs.kdeconnect.enable = true;
