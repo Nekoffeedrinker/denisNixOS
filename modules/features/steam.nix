321111b63638b97025e08a6b504829285ead4033
Denis Castillón <orangeneko158@outlook.com>
2026-04-28 17:49:51 -0700

    steam: fix blackscreen en Niri y mejoras
    En Niri, Steam se abria mostrando una pantalla negra, con el overlay se
    debería de solucionar.
    
    Tambien se añadió winetriks (porque lo vi en el overlay) y se agregaron
    comentarios para mejorar la documentación.

--- modules/features/steam.nix
+++ modules/features/steam.nix
@@ -1,9 +1,23 @@
 {self, ...}: {
   flake.nixosModules.steam = {pkgs, ...}: {
     imports = [self.nixosModules.graficos];
-    programs.steam.enable = true;
-    programs.steam.gamescopeSession.enable = true;
-    programs.gamemode.enable = true;
+
+    # Arreglar la pantalla negra cuando se abre en Niri
+    nixpkgs.overlays = [
+      (final: prev: {
+        steam = prev.steam.override {
+          extraArgs = "-cef-disable-gpu-compositing";
+        };
+      })
+    ];
+
+    # Instalar steam
+    programs.steam = {
+      enable = true;
+      protontricks.enable = true;
+      gamescopeSession.enable = true; # Por si hay problemas de escalado o resolución
+    };
+    programs.gamemode.enable = true; # Demonio para mejorar rendimiento
 
     environment.systemPackages = with pkgs; [
       mangohud # monitoreo de fps
@@ -14,5 +28,10 @@
     environment.sessionVariables = {
       STEAM_EXTRA_COMPAT_TOOLS_PATHS = "\${HOME}/.steam/root/compatibilitytools.d";
     };
+
+    /*
+    Para utilizar gamescope, gamemode y mangohud, se deben declarar en las
+    opciones de lanzamiento generales o de un juego específico.
+    */
   };
 }
