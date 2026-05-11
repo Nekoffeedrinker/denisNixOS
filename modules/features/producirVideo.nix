74fb2b894f24b8e2e13e9254b449624fa4938dcd
Denis Castillón <orangeneko158@outlook.com>
2026-05-11 10:05:01 -0700

    add: Subtitle Edit (como archivo)
    Se añadió el flatpak de Subtitle Edit pero descargado de manera manual.

--- modules/features/producirVideo.nix
+++ modules/features/producirVideo.nix
@@ -12,6 +12,11 @@
         appId = "no.mifi.losslesscut";
         origin = "flathub";
       }
+      {
+        appId = "dk.nikse.subtitleedit";
+        sha256 = "6d8e584074bf1aec68eedb65e3c119f3545f2daeb3105367fd5a5db7e694656b";
+        bundle = "file:///home/denis/denisNixOS/modules/features/flatpak-bundle/SubtitleEdit-linux-x64.flatpak";
+      }
     ];
   };
 }
