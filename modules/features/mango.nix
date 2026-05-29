1ed21b967911386d2ce39f79547ebc0c041b4849
Denis Castillón <orangeneko158@outlook.com>
2026-05-28 21:52:31 -0700

    fix: no cargaba GDM por exeso de portals

--- modules/features/mango.nix
+++ modules/features/mango.nix
@@ -1,5 +1,9 @@
 {inputs, ...}: {
-  flake.nixosModules.mango = {pkgs, ...}: {
+  flake.nixosModules.mango = {
+    pkgs,
+    lib,
+    ...
+  }: {
     imports = [
       inputs.mangowm.nixosModules.mango
     ];
@@ -8,11 +12,20 @@
 
     # activar portal (interfaz para que apps en
     # Wayland accedan a archivos, pantalla, etc.)
-    xdg.portal.enable = true;
-    xdg.portal.extraPortals = with pkgs; [
-      xdg-desktop-portal-gtk
+    # --------------------------------------------
+    # xdg.portal.enable = true;
+    # xdg.portal.extraPortals = with pkgs; [
+    #   xdg-desktop-portal-gtk
+    # ];
+    # security.polkit.enable = true;
+
+    # Overridear los portales que el módulo de mango jala automáticamente
+    # para no entrar en conflicto con GNOME/niri
+    xdg.portal.extraPortals = lib.mkForce [
+      pkgs.xdg-desktop-portal-wlr
+      pkgs.xdg-desktop-portal-gtk
     ];
-    security.polkit.enable = true;
+    xdg.portal.wlr.enable = lib.mkForce false;
 
     environment.systemPackages = [
       pkgs.xwayland-satellite
