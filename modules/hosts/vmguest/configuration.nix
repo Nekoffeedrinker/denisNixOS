60dd11f360b140f06b01f3aaaee6a2241a4b1db3
Denis Castillón <orangeneko158@outlook.com>
2026-05-31 13:51:16 -0700

    doc: formatear con alejandra

--- modules/hosts/vmguest/configuration.nix
+++ modules/hosts/vmguest/configuration.nix
@@ -13,140 +13,137 @@
 
     # Acá inicia la configuración de nix -------------------------------------
 
-  # Bootloader.
-  boot.loader.grub.enable = true;
-  boot.loader.grub.device = "/dev/vda";
-  boot.loader.grub.useOSProber = true;
+    # Bootloader.
+    boot.loader.grub.enable = true;
+    boot.loader.grub.device = "/dev/vda";
+    boot.loader.grub.useOSProber = true;
 
-  # Virtualización
-  services.spice-vdagentd.enable = true;
-  services.qemuGuest.enable = true;
+    # Virtualización
+    services.spice-vdagentd.enable = true;
+    services.qemuGuest.enable = true;
 
+    networking.hostName = "vmguest"; # Define your hostname.
+    # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
-  networking.hostName = "vmguest"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+    # Configure network proxy if necessary
+    # networking.proxy.default = "http://user:password@proxy:port/";
+    # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+    # Enable networking
+    networking.networkmanager.enable = true;
 
-  # Enable networking
-  networking.networkmanager.enable = true;
+    # Set your time zone.
+    time.timeZone = "America/Mazatlan";
 
-  # Set your time zone.
-  time.timeZone = "America/Mazatlan";
+    # Select internationalisation properties.
+    i18n.defaultLocale = "es_MX.UTF-8";
 
-  # Select internationalisation properties.
-  i18n.defaultLocale = "es_MX.UTF-8";
+    # Enable the X11 windowing system.
+    # You can disable this if you're only using the Wayland session.
+    services.xserver.enable = true;
 
-  # Enable the X11 windowing system.
-  # You can disable this if you're only using the Wayland session.
-  services.xserver.enable = true;
+    # Enable the KDE Plasma Desktop Environment.
+    services.displayManager.sddm.enable = true;
+    services.desktopManager.plasma6.enable = true;
 
-  # Enable the KDE Plasma Desktop Environment.
-  services.displayManager.sddm.enable = true;
-  services.desktopManager.plasma6.enable = true;
+    # Configure keymap in X11
+    services.xserver.xkb = {
+      layout = "us";
+      variant = "altgr-intl";
+    };
 
-  # Configure keymap in X11
-  services.xserver.xkb = {
-    layout = "us";
-    variant = "altgr-intl";
-  };
+    # Enable CUPS to print documents.
+    services.printing.enable = true;
 
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
+    # Enable sound with pipewire.
+    services.pulseaudio.enable = false;
+    security.rtkit.enable = true;
+    services.pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+      # If you want to use JACK applications, uncomment this
+      #jack.enable = true;
 
-  # Enable sound with pipewire.
-  services.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
+      # use the example session manager (no others are packaged yet so this is enabled by default,
+      # no need to redefine it in your config for now)
+      #media-session.enable = true;
+    };
 
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
-  };
+    # Enable touchpad support (enabled default in most desktopManager).
+    # services.xserver.libinput.enable = true;
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
+    # Define a user account. Don't forget to set a password with ‘passwd’.
+    users.users.denis = {
+      isNormalUser = true;
+      description = "Denis Pilar";
+      extraGroups = ["networkmanager" "wheel"];
+      packages = with pkgs; [
+        kdePackages.kate
+        #  thunderbird
+      ];
+    };
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.denis = {
-    isNormalUser = true;
-    description = "Denis Pilar";
-    extraGroups = [ "networkmanager" "wheel" ];
-    packages = with pkgs; [
-      kdePackages.kate
-    #  thunderbird
+    # Install firefox.
+    programs.firefox.enable = true;
+
+    # Allow unfree packages
+    nixpkgs.config.allowUnfree = true;
+
+    # List packages installed in system profile. To search, run:
+    # $ nix search wget
+    environment.systemPackages = with pkgs; [
+      wget
+      neovim
+      git
+      stow
+      lazygit
+      # Utilidades nix
+      alejandra
+      nixd
+      nh
+      nix-output-monitor
+      nvd
+      # Cosas de la maquina virtual
+      spice-vdagent
     ];
-  };
 
-  # Install firefox.
-  programs.firefox.enable = true;
+    # Some programs need SUID wrappers, can be configured further or are
+    # started in user sessions.
+    # programs.mtr.enable = true;
+    # programs.gnupg.agent = {
+    #   enable = true;
+    #   enableSSHSupport = true;
+    # };
 
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
+    # List services that you want to enable:
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
-    wget
-    neovim
-    git
-    stow
-    lazygit
-    # Utilidades nix
-    alejandra
-    nixd
-    nh
-    nix-output-monitor
-    nvd
-    # Cosas de la maquina virtual
-    spice-vdagent
-  ];
+    # Enable the OpenSSH daemon.
+    # services.openssh.enable = true;
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
+    # Open ports in the firewall.
+    # networking.firewall.allowedTCPPorts = [ ... ];
+    # networking.firewall.allowedUDPPorts = [ ... ];
+    # Or disable the firewall altogether.
+    # networking.firewall.enable = false;
 
-  # List services that you want to enable:
+    # ==================== Variables de entorno ====================
 
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
+    # Ubicación del flake (necesario para nh)
+    environment.sessionVariables = {
+      NH_FLAKE = "/home/denis/denisNixOS/";
+    };
 
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
+    # ==================== Sistem state version ====================
 
-
-  # ==================== Variables de entorno ====================
-  
-  # Ubicación del flake (necesario para nh)
-  environment.sessionVariables = {
-    NH_FLAKE = "/home/denis/denisNixOS/";
-  };
-
-  # ==================== Sistem state version ====================
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "25.11"; # Did you read the comment?
-
+    # This value determines the NixOS release from which the default
+    # settings for stateful data, like file locations and database versions
+    # on your system were taken. It‘s perfectly fine and recommended to leave
+    # this value at the release version of the first install of this system.
+    # Before changing this value read the documentation for this option
+    # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+    system.stateVersion = "25.11"; # Did you read the comment?
 
     # Acá termina la config de nix --------------------------------------------
   };
--- modules/hosts/vmguest/hardware.nix
+++ modules/hosts/vmguest/hardware.nix
@@ -5,23 +5,24 @@
     modulesPath,
     ...
   }: {
-  imports =
-    [ (modulesPath + "/profiles/qemu-guest.nix")
+    imports = [
+      (modulesPath + "/profiles/qemu-guest.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "ahci" "xhci_pci" "virtio_pci" "sr_mod" "virtio_blk" ];
-  boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ "kvm-intel" ];
-  boot.extraModulePackages = [ ];
+    boot.initrd.availableKernelModules = ["ahci" "xhci_pci" "virtio_pci" "sr_mod" "virtio_blk"];
+    boot.initrd.kernelModules = [];
+    boot.kernelModules = ["kvm-intel"];
+    boot.extraModulePackages = [];
 
-  fileSystems."/" =
-    { device = "/dev/disk/by-uuid/a1b0e12d-fc4a-463a-813d-d201b347b303";
+    fileSystems."/" = {
+      device = "/dev/disk/by-uuid/a1b0e12d-fc4a-463a-813d-d201b347b303";
       fsType = "ext4";
     };
 
-  swapDevices =
-    [ { device = "/dev/disk/by-uuid/1e797e68-0225-4ebb-bce3-8c36383feee8"; }
+    swapDevices = [
+      {device = "/dev/disk/by-uuid/1e797e68-0225-4ebb-bce3-8c36383feee8";}
     ];
 
-  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+    nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  };
 }
