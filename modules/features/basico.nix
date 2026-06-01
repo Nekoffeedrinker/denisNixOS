aa5f057c4926eb27f4a4f30b37415767ba34ae03
Denis Castillón <orangeneko158@outlook.com>
2026-05-31 22:36:25 -0700

    Vincular básico e indispensable

--- modules/features/basico.nix
+++ modules/features/basico.nix
@@ -2,7 +2,7 @@
   flake.nixosModules.basicos = {pkgs, ...}: {
     # =============== Importar ===============
     imports = [
-      self.nixosModules.nixUtil
+      self.nixosModules.indispensable
       self.nixosModules.zsh
     ];
 
@@ -11,56 +11,15 @@
     # Imprimir documentos usando CUPS
     services.printing.enable = true;
 
-    # Habilitar sonido con pipewire.
-    services.pulseaudio.enable = false;
-    security.rtkit.enable = true;
-    services.pipewire = {
-      enable = true;
-      alsa.enable = true;
-      alsa.support32Bit = true;
-      pulse.enable = true;
-      # If you want to use JACK applications, uncomment this
-      #jack.enable = true;
-
-      # use the example session manager (no others are packaged yet so this is enabled by default,
-      # no need to redefine it in your config for now)
-      #media-session.enable = true;
-    };
-
     # Comunicarse con reproductores de medio (atajos para pausar y reproducir)
     services.playerctld.enable = true;
 
-    # =============== Localización ===============
-
-    # Lenguaje del sistema y formato de fecha y hora
-    i18n.defaultLocale = "es_MX.UTF-8";
-
-    # Zona horaria
-    time.timeZone = "America/Mazatlan";
-
-    # =============== Seguridad ===============
-
-    # Mostrar caracteres de contraseña en el sudo
-    security.sudo.extraConfig = ''
-      Defaults pwfeedback
-    '';
-
     # =============== Paquetes ===============
     environment.systemPackages = with pkgs; [
-      wget
-
       # Terminal (cli)
       kitty
-      neovim
-      git
-      lazygit
       delta # pager de git diff
-      stow
-      fastfetch
-      hyfetch # pride fetch
-      # herramientas
       opencode
-      btop # monitor de recursos
       tree # arbol de directorios
       fzf # búsqueda chida
       bat # cat mejorado
@@ -87,32 +46,6 @@
       gparted
       localsend
       mission-center
-    ];
-
-    # Instalar firefox.
-    programs.firefox.enable = true;
-
-    # Tipografías
-    fonts.packages = with pkgs; [
-      nerd-fonts.jetbrains-mono
-      monaspace
-      inter
-      public-sans
-      libertinus
-      courier-prime
-      roboto
-      roboto-serif
-      roboto-slab
-      corefonts
-
-      # Multiidioma y compatibilidad
-      noto-fonts
-      noto-fonts-cjk-sans
-      noto-fonts-cjk-serif
-      noto-fonts-cjk-sans-static
-      noto-fonts-cjk-serif-static
-      noto-fonts-color-emoji
-      noto-fonts-monochrome-emoji
     ];
   };
 }
