0c3fcf254622cb48118a03b3ce70215ebf32d013
Denis Castillón <orangeneko158@outlook.com>
2026-06-11 23:44:40 -0700

    affinity: volver a la 3

--- modules/features/affinity.nix
+++ modules/features/affinity.nix
@@ -2,10 +2,10 @@
   flake.nixosModules.affinity = {pkgs, ...}: {
     nixpkgs.overlays = [inputs.affinity-nix.overlays.default];
     environment.systemPackages = with pkgs; [
-      # affinity-v3
-      affinity-designer
-      affinity-photo
-      affinity-publisher
+      affinity-v3
+      # affinity-designer
+      # affinity-photo
+      # affinity-publisher
     ];
   };
 }
