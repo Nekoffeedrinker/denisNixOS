1df619bf04bfcd07f67959dae1f65263bff5083a
Denis Castillón <orangeneko158@outlook.com>
2026-07-11 14:29:28 -0700

    add: Cinnamon DE

---
+++ modules/features/desktopEnviroments/cinnamon.nix
@@ -0,0 +1,25 @@
+{...}: {
+  flake.nixosModules.cinnamon = {pkgs, ...}: {
+    # Instalar el entorno de escritorio Cinnamon
+    services.xserver.desktopManager.cinnamon.enable = true;
+
+    # # Display Manager de Cinnamon
+    # services.xserver.displayManager.lightdm.enable = true;
+
+    # Instalar dependencias y paquetes extra
+    environment.systemPackages = with pkgs; [
+      snapshot # App de cámara de GNOME
+      gnome-system-monitor # Monitor de tareas de GNOME
+
+      nemo-preview # Preview de archivos
+      xclip # clipboard para temrinal
+
+      # Dependencias del applet: Automatic dark/light themes
+      gnumake
+      gcc
+    ];
+
+    # Clipboard manager
+    programs.gpaste.enable = true;
+  };
+}
