f9b0aab5dd4a6387645aae1583383c3d32f3b074
Denis Castillón <orangeneko158@outlook.com>
2026-08-02 16:10:17 -0700

    noctaliaV5: evitar que se compile en local

--- flake.nix
+++ flake.nix
@@ -31,7 +31,7 @@
     };
 
     noctalia = {
-      url = "github:noctalia-dev/noctalia";
+      url = "github:noctalia-dev/noctalia/cachix";
     };
 
     affinity-nix.url = "github:mrshmllow/affinity-nix";
--- modules/features/desktopEnviroments/noctaliaV5.nix
+++ modules/features/desktopEnviroments/noctaliaV5.nix
@@ -4,6 +4,12 @@
       inputs.noctalia.nixosModules.default
     ];
 
+    # Usar la caché del binario en lugar de compiar en local
+    nix.settings = {
+      extra-substituters = ["https://noctalia.cachix.org"];
+      extra-trusted-public-keys = ["noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="];
+    };
+
     programs.noctalia = {
       enable = true;
       recommendedServices.enable = true;
